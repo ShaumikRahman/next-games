dafd201dea8d60bf742e6f563659adb853c5b64d
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2021-10-27 21:19:41 +0100

    initial API calls, initial layout of data

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
 import styles from '../styles/Home.module.scss'
 
-export default function Home() {
+export default function Home({ data }) {
+
+  console.log(data);
+
   return (
     <div className="container">
       <h1 className="title">
         Next Games
       </h1>
+      <div className={styles.games}>
+        {data.results.map((game, index) => {
+          return <div className={styles.game} key={index}>
+            <h2>
+              {game.name}
+            </h2>
+            <img className={styles.image} src={game.background_image} alt={game.slug} />
+          </div>
+        })}
+      </div>
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+  const response = await fetch(`https://rawg.io/api/games?key=${process.env.API_KEY}&page_size=10`);
+  const data = await response.json();
+
+  return {
+    props: {
+      data
+    },
+    revalidate: 30
+  }
 }
