73b80da20992b35420634125fdbb5808eb831d56
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2021-11-25 19:18:25 +0000

    correct routing after screenshots

--- pages/screenshots/[slug].js
+++ pages/screenshots/[slug].js
@@ -1,10 +1,14 @@
 import styles from "../../styles/Screenshots.module.scss";
 import Link from "next/link";
 import Head from "next/head";
+import Empty from "../../components/Empty";
+import { useRouter } from "next/router";
 
 export default function Screenshots({ game, screenshots }) {
 
-  console.log(game);
+  const router = useRouter();
+
+  const from = router.query.from;
 
   return (
     <div className="container">
@@ -12,15 +16,15 @@ export default function Screenshots({ game, screenshots }) {
         <title>{`Screenshots for ${game.name}`}</title>
         <meta name="description" content={`Screenshots for ${game.name}`} />
       </Head>
-      <Link href={`/game/${game.slug}`}>
+      <Link href={`/game/${game.slug}?from=${from}`} passHref>
         <h1 className={styles.return}>Game</h1>
       </Link>
       <div className={styles.game}>
         <h1 className={styles.title}>{`${game.name} screenshots`}</h1>
         <div className={styles.screenshots}>
-        {screenshots.results.map(screenshot => {
+        {screenshots.results ? screenshots.results.map(screenshot => {
             return <img className={styles.screenshot} key={screenshot.id} src={screenshot.image} alt={game.slug} />
-          })}
+          }) : <Empty />}
         </div>
       </div>
     </div>
@@ -28,6 +32,8 @@ export default function Screenshots({ game, screenshots }) {
 }
 
 export async function getServerSideProps(context) {
+
+  console.log(context.query.slug);
 
   const gameResponse = await fetch(`https://rawg.io/api/games/${context.query.slug}?key=${process.env.KEY}`);
 
